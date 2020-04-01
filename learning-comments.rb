c5e9980e8fd024f16e2d16ac9052f1c020cb6db4
distributed-memory-9073 <noahshafer0211@gmail.com>
2020-04-01 17:48:13 +0000

    Automatically backed up by Learn

--- learning-comments.rb
+++ learning-comments.rb
@@ -12,8 +12,8 @@ puts "All the boys and girls"
 # puts "Joy to the fishes in the deep blue sea"
 puts "Joy to you and me"
 
-# name = "Byron"
-name = "Luca"
+name = "Byron"
+# name = "Luca"
  
 puts "We're sorry, but per health inspector's rules, #{name} is not allowed in
 the store."
