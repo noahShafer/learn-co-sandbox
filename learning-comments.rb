05210155ca946efb4819c8d3123b1f585f347b09
distributed-memory-9073 <noahshafer0211@gmail.com>
2020-04-01 17:46:54 +0000

    Automatically backed up by Learn

--- learning-comments.rb
+++ learning-comments.rb
@@ -4,7 +4,7 @@
 # Perform a constant expression evaluation
 3
 # Assign constant 3 to bare-word variable triangle_sides
-`triangle_sides`
+triangle_sides = `triangle_sides`
 
 # From the Three Dog Night song: "joy to the World (Jeremiah was a Bullfrog)"
 puts "Joy to the world"
