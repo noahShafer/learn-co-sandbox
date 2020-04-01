07d7c4d6e2893613fad36191bce1f68a971dfb4e
distributed-memory-9073 <noahshafer0211@gmail.com>
2020-04-01 17:47:57 +0000

    Automatically backed up by Learn

--- learning-comments.rb
+++ learning-comments.rb
@@ -11,3 +11,11 @@ puts "Joy to the world"
 puts "All the boys and girls"
 # puts "Joy to the fishes in the deep blue sea"
 puts "Joy to you and me"
+
+# name = "Byron"
+name = "Luca"
+ 
+puts "We're sorry, but per health inspector's rules, #{name} is not allowed in
+the store."
+ 
+#=> We're sorry, but per health inspector's rules, Luca is not allowed in the store.
