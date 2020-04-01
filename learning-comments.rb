3e6a230dad38d21206a57fd0d0dcde3323c3f2fe
distributed-memory-9073 <noahshafer0211@gmail.com>
2020-04-01 17:45:47 +0000

    Automatically backed up by Learn

--- learning-comments.rb
+++ learning-comments.rb
@@ -1,2 +1,13 @@
 # Don't do this:
 puts ( 1 #+ 1)
+
+# Perform a constant expression evaluation
+3
+# Assign constant 3 to bare-word variable triangle_sides
+`triangle_sides`
+
+# From the Three Dog Night song: "joy to the World (Jeremiah was a Bullfrog)"
+puts "Joy to the world"
+puts "All the boys and girls"
+puts "Joy to the fishes in the deep blue sea"
+puts "Joy to you and me"
